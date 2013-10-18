4ba80b63af17c6bd11968e259e92e1350e90219a
peiliping <peilipingplp@gmail.com>
2013-10-18 16:18:40 +0800

    update

--- web-web/src/main/java/com/peiliping/web/controls/TestControl.java
+++ web-web/src/main/java/com/peiliping/web/controls/TestControl.java
@@ -4,6 +4,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class TestControl {
@@ -14,5 +15,15 @@ public class TestControl {
 	 public String test(){
 		logger.error("FFFF");
 		 return "test";
+	 }
+	
+	@RequestMapping("/testfm.htm")
+	 public ModelAndView testfm(){
+		logger.error("testfm");
+		
+		ModelAndView mv = new ModelAndView("testfm");
+        mv.addObject("name", "My First Spring Mvc");
+		
+		 return mv;
 	 }
 }
