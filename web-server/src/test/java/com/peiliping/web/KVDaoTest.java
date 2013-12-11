eae2d5d55f398f655273b84b224ddc41658075ec
peiliping <peilipingplp@gmail.com>
2013-12-11 15:45:56 +0800

    update

---
+++ web-server/src/test/java/com/peiliping/web/KVDaoTest.java
@@ -0,0 +1,24 @@
+package com.peiliping.web;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import com.peiliping.web.dao.KVDAO;
+
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations={"file:src/test/resources/test-dbsource.xml"})
+public class KVDaoTest {
+	@Autowired
+	private KVDAO kvDAO;
+
+	@Test
+	public void test_KV(){
+		System.out.println(kvDAO.getKV("1").getK());
+	}	
+	
+	
+}
