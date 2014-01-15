2544912962b6cf81dd9d1534412ede8b54604334
peiliping <peilipingplp@gmail.com>
2014-01-15 18:04:18 +0800

    update

--- web-server/src/main/java/com/peiliping/web/server/datamerge/AbstractData.java
+++ web-server/src/main/java/com/peiliping/web/server/datamerge/AbstractData.java
@@ -4,30 +4,12 @@ import lombok.Getter;
 
 public abstract class AbstractData {
 	@Getter
-	protected long id ;
-	@Getter
 	protected String name ;  //保存在MAP的key
-	@Getter
-	protected String type ;
-	@Getter
-	protected double count ;//次数
-	@Getter
-	protected double cost ;//消耗时间
-	@Getter
-	protected double max ;
-	@Getter
-	protected double min ;
 	@Getter
 	protected long expiretime ;//到期时间点
 	
-	public AbstractData(long id,String name,String type,double count,double cost,double max,double min,long expiretime) {
-		this.id = id ;
+	public AbstractData(String name,long expiretime) {
 		this.name = name ;
-		this.type = type ;
-		this.count = count;
-		this.cost = cost ;
-		this.max = max ;
-		this.min = min ;
 		this.expiretime = expiretime ;
 	}
 	
--- web-server/src/main/java/com/peiliping/web/server/datamerge/TimeDivisionData.java
+++ web-server/src/main/java/com/peiliping/web/server/datamerge/TimeDivisionData.java
@@ -4,7 +4,7 @@ public abstract class TimeDivisionData extends AbstractData {
 	
 	public TimeDivisionData(long id, String name, String type, double count,
 			double cost, double max, double min,long timestamp,long division) {
-		super(id, name, type, count, cost, max, min, ((timestamp/division + 1)*division));
+		super(name, ((timestamp/division + 1)*division));
 		super.name = super.name +  timestamp/division;
 	}
 
