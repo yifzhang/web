dba69462097f5efe776cab8a65712e7d7f942f48
peiliping <peilipingplp@gmail.com>
2014-04-29 09:43:48 +0800

    update

--- build.sh
+++ build.sh
@@ -1,4 +1,5 @@
 mvn clean
 mvn package -U -Dmaven.test.skip=true assembly:assembly
 rm -rf ~/dev/tomcat7/webapps/ROOT/*
-unzip web-web/target/web.war -d ~/dev/tomcat7/webapps/ROOT/
+tar -zxvf target/web.tar.gz -C ~/dev/tomcat7/webapps/
+mv ~/dev/tomcat7/webapps/web.war ~/dev/tomcat7/webapps/web
